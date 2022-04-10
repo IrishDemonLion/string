0726b31c4af07c1738aa09e8f317f34dcafca87e
IrishDemonLion <88555320+IrishDemonLion@users.noreply.github.com>
2022-04-10 18:57:43 -0500

    updated

--- src/ParseStrings.java
+++ src/ParseStrings.java
@@ -1,38 +1,36 @@
 import java.util.Scanner;
 
 public class ParseStrings {
-    public static void main(String[]args){
+
+    public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
-        Scanner inputword;
-        String inputline = "";
-        String substring1 = "";
-        String substring2 = "";
-        System.out.println("Enter input string: ");
 
-        while (!inputline.matches("q")){
-            inputline = scnr.nextLine();
-            inputline = inputline.replace(",",", ");
-            inputword = new Scanner(inputline);
-            int commaPos = inputline.indexOf(",");
+        String userInput = getUserInput(scnr);
+        while (!userInput.equals("q")) {
 
-            if (commaPos <= -1) && (!inputline.matches("q") == false){
+            if (userInput.contains(",")) {
+                showWordsIn(userInput);
+            } else {
                 System.out.println("Error: No comma in string");
-                System.out.println("Enter input string: ");
             }
-            else if ((commaPos <= -1) && (inputline.length() ==0 || inputline.split("\\s+").length < 2) && (inputline.matches("q") == false)) {
-                System.out.println("Please enter two words");
-                System.out.println("Enter input string: ");
-                else if (inputline.matches("q") == false){
-                    substring1 = inputword.next();
-                    substring2 = inputword.nextLine();
 
-                    System.out.println("First word: " + substring1.replaceAll("\\s",""));
-                    System.out.println("Second word: " + substring2.replaceAll("\\s","").replaceAll("\\W",""));
+            System.out.println();
 
-                    System.out.println("\n");
-                    System.out.println("Enter input string: ");
-                }
-            }
+            userInput = getUserInput(scnr);
         }
+    }
+
+    private static void showWordsIn(String userInput) {
+        int index = userInput.indexOf(",");
+        String firstWord = userInput.substring(0, index).trim();
+        String secondWord = userInput.substring(index + 1).trim();
+
+        System.out.printf("First word: %s%n", firstWord);
+        System.out.printf("Second word: %s%n", secondWord);
+    }
+
+    private static String getUserInput(Scanner scnr) {
+        System.out.println("Enter input string: ");
+        return scnr.nextLine();
     }
 }
